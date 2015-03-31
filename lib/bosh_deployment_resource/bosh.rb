3011bd73f6d6e30390efe9f362865a8c183ccc5a
Chris Brown <cbrown@pivotal.io>
2015-03-31 14:58:39 -0700

    add class that class out to bosh

--- lib/bosh_deployment_resource/bosh.rb
+++ lib/bosh_deployment_resource/bosh.rb
@@ -1,9 +1,37 @@
+require "open3"
+
 module BoshDeploymentResource
   class Bosh
+    def initialize(target, username, password)
+      @target = target
+      @username = username
+      @password = password
+    end
+
     def upload_stemcell(path)
+      bosh("upload stemcell #{path} --skip-if-exists")
     end
 
     def upload_release(path, rebase=false)
+      command = "upload release #{path} --skip-if-exists"
+      command << " --rebase" if rebase
+
+      bosh(command)
+    end
+
+    private
+
+    attr_reader :target, :username, :password
+
+    def bosh(command)
+      run("bosh -n -t #{target} -u #{username} -p #{password} #{command}")
+    end
+
+    def run(command)
+      pid = Process.spawn(command, out: :out, err: :err)
+      Process.wait(pid)
+
+      raise "command '#{command} failed!" unless $?.success
     end
   end
 end
